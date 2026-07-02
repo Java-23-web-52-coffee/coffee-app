ae57a2801fac5947eb13e918a94ef2bf37e82178
Tim Byers <timothy.byers007@gmail.com>
2026-07-02 09:40:34 -0600

    turned off dark mode

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import {ThemeInit} from "../.flowbite-react/init";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -33,6 +34,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
+      <ThemeInit />
         {children}
         <ScrollRestoration />
         <Scripts />
