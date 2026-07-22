aa0051f8011341b531a4169ffb6fc1ac9497d9a9
Tim Byers <timothy.byers007@gmail.com>
2026-07-22 16:02:14 -0600

    worked on get shop by id

--- backend/src/apis/shop/shop.model.ts
+++ backend/src/apis/shop/shop.model.ts
@@ -5,4 +5,3 @@ import {sql} from "../../utils/database.utils.ts";
 export async function selectAllShops (): Promise<Shop[]> {
     const rowList = await sql`SELECT id, address, hours, lat, lng, name, phone, image_url FROM shop`
     return ShopSchema.array().parse(rowList)
-}
